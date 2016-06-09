549bff7bcfcf816f48916be415116d852ebd0fc9
Adams, Stormy (sa836x) <sa836x@us.att.com>
2016-06-09 10:43:05 -0500

    added a bunch of comments to try and concrete the learning and explain what each thing is doing

--- layer.py
+++ layer.py
@@ -1,26 +1,27 @@
 from artificial_network_utils import make_matrix
 from artificial_network_utils import between
 
-use_bias = 1
 
 class Layer:
 
     def __init__(self, id, layer_size, prev_layer_size):
 
-        self.id = id
-        self.n_neurons = layer_size
-        self.bias_val = 1
+        self.id = id  # this is to help keep track of what layer we are looking at.
+        self.n_neurons = layer_size  # how many neurons are in the current layer.
+        self.bias_val = 1  # this is the default value for the bias used in order to normalize the threshold
 
         self.input = [0] * self.n_neurons
 
-        self.output = [0] * (self.n_neurons + use_bias)
+        self.output = [0] * (self.n_neurons + self.bias_val)
 
         self.output[0] = self.bias_val
 
         self.error = [0] * self.n_neurons
 
-        self.weight = make_matrix(prev_layer_size + use_bias, self.n_neurons)
+        # make_matrix will generate an N x M matrix
+        self.weight = make_matrix(prev_layer_size + self.bias_val, self.n_neurons)
 
+        # This will populate the weight N x M array with randomized values from -0.2 to 0.2
         for i in range(len(self.weight)):
             for j in range(len(self.weight[i])):
                 self.weight[i][j] = between(-0.2,0.2)
