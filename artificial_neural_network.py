f36e7b5ded8b9531dda29dd8bb4bb8b418b2b97a
Adams, Stormy (sa836x) <sa836x@us.att.com>
2016-06-09 10:02:47 -0500

    added general application structure

---
+++ artificial_neural_network.py
@@ -0,0 +1,14 @@
+class ArtificialNeuralNetwork:
+
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
+
+    def predict(self):
+        pass
+
+    def update_weights(self):
+        pass
+
--- main.py
+++ main.py
@@ -1,21 +1,3 @@
-from app import compute_neural_output
+from artificial_neural_network import ArtificialNeuralNetwork
+from layer import Layer
 
-samples = [
-    {
-        "input": -2,
-        "weight": 1,
-        "output": 0
-    },
-    {
-        "input": 3,
-        "weight": 1,
-        "output": 1
-    },
-    {
-        "input": 0,
-        "weight": -2,
-        "output": 0
-    }
-]
-
-print compute_neural_output(samples, .4)
